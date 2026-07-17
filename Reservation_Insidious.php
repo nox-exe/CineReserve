9389c1a2b4b924c0509bdb8284fa31654ced66d4
Kimm1m <149302183+Kimm1m@users.noreply.github.com>
2026-07-18 06:45:37 +0800

    Add files via upload

---
+++ Reservation_Insidious.php
@@ -0,0 +1,144 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Reservation.css">
+    <title>CineReserve</title>
+</head>
+<body>
+    <nav class="Navigation">
+        <a href="Home.html">
+        <img src="" class="Logo">
+        </a>
+    <ul>
+
+        <li>
+        <img src="Assets/UI-icons/Reservation.png" class="Reservation-icon" width="30px">
+        <a href="Reservation_list.php">
+        Reservation
+        </a>
+        </li>
+
+        <li>
+        <img src="Assets/UI-icons/Movie.png" class="Movie-icon" width="30px">
+        <a href="Upcoming.php">
+        Upcoming Movies
+        </a>
+        </li>
+
+        <li>
+        <img src="Assets/UI-icons/Screening.png" class="Screening-icon" width="29px">
+        <a href="Screening.php">
+        Screening
+        </a>
+        </li>
+
+        <li>
+        <img src="Assets/UI-icons/Locations.png" class="Locations-icon" width="29px">
+        <a href="Locations.php">
+        Theather Locations
+        </a>
+        </li>
+
+        <li class="Logout">
+        <img src="Assets/UI-icons/Logout.png" class="Logout-icon" width="26px">
+        <a href="Login.php">
+        Log Out
+        </a>
+        </li>
+
+    </ul>
+    </nav>
+    <main class="Main">
+
+<h2 class="title">Reservation</h2>
+<section class="reservation-container">
+
+    <section class="First-Movie">
+
+        <section class="First">
+
+        <div class="header">
+
+            <div class="First-img-container">
+                <img src="Assets/Movie-posters/Insidious.jpg">
+            </div>
+
+            <div class="Movie-info">
+                    <h2>Insidious: Out of the Further</h2>
+                    <p>
+                        A trio of stalkers infiltrate a quiet suburb and force a new family into the astral plane, where they uncover a terrifying truth: the Further is bleeding into the real world.
+            </div>
+
+            <div class="Price-container">Price
+            </div>
+        </div>
+
+    <div class="body-container">
+
+    <div class="details">
+        <h2>CineReserve Cubao</h2>
+        
+        <div class="time-container">
+        <label for="time">Choose Time:</label>
+
+        <select id="time" name="time">
+            <option value="1:30 PM">1:30 - 3:00 PM</option>
+            <option value="3:30 PM">3:30 - 6:00 PM</option>
+            <option value="6:30 PM">6:30 - 8:00 PM</option>
+        </select>
+        </div>
+
+<div class="seat-container">
+
+    <h2>Choose Seating</h2>
+
+    <!-- ROW -->
+    <label>Row:</label>
+    <select id="row">
+        <option value="A">Row A</option>
+        <option value="B">Row B</option>
+        <option value="C">Row C</option>
+        <option value="D">Row D</option>
+        <option value="E">Row E</option>
+    </select>
+
+
+    <!-- COLUMN -->
+    <label>Seat Number:</label>
+    <select id="column">
+        <option value="1">Seat 1</option>
+        <option value="2">Seat 2</option>
+        <option value="3">Seat 3</option>
+        <option value="4">Seat 4</option>
+        <option value="5">Seat 5</option>
+        <option value="6">Seat 6</option>
+        <option value="7">Seat 7</option>
+        <option value="8">Seat 8</option>
+    </select>
+
+
+    <button class="confirm-seat">
+        Confirm Seat
+    </button>
+
+</div>
+
+    </div>
+
+        <div class="Reserve-container">
+            
+        </div>
+
+    </div>
+        </section>
+
+    </section>
+
+</section>
+    </main>
---
+++ Reservation_list.php
@@ -0,0 +1,201 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Reservation_List.css">
+    <title>CineReserve</title>
+</head>
+<body>
+    <nav class="Navigation">
+        <a href="Home.html">
+        <img src="" class="Logo">
+        </a>
+    <ul>
+
+        <li>
+        <img src="Assets/UI-icons/Reservation.png" class="Reservation-icon" width="30px">
+        <a href="Reservation_list.php">
+        Reservation
+        </a>
+        </li>
+
+        <li>
+        <img src="Assets/UI-icons/Movie.png" class="Movie-icon" width="30px">
+        <a href="Upcoming.php">
+        Upcoming Movies
+        </a>
+        </li>
+
+        <li>
+        <img src="Assets/UI-icons/Screening.png" class="Screening-icon" width="29px">
+        <a href="Screening.php">
+        Screening
+        </a>
+        </li>
+
+        <li>
+        <img src="Assets/UI-icons/Locations.png" class="Locations-icon" width="29px">
+        <a href="Locations.php">
+        Theather Locations
+        </a>
+        </li>
+
+        <li class="Logout">
+        <img src="Assets/UI-icons/Logout.png" class="Logout-icon" width="26px">
+        <a href="Login.php">
+        Log Out
+        </a>
+        </li>
+
+    </ul>
+    </nav>
+    <main class="Main">
+
+<h2 class="Reservation-title">Reservation List</h2>
+<section class="reservation-container">
+
+    <!-- FIRST MOVIE -->
+    <section class="First-Movie">
+
+        <section class="First">
+
+            <div class="First-timer">
+
+            </div>
+
+            <div class="First-img-container">
+                <img src="Assets/Movie-posters/Minions.webp">
+            </div>
+
+        </section>
+
+        <div class="Reserve-container">
+            <h2>
+                <a href="Reservation_Minions.php">RESERVE</a>
+            </h2>
+        </div>
+
+    </section>
+
+    <!-- SECOND MOVIE -->
+    <section class="Second-Movie">
+
+        <section class="Second">
+
+            <div class="Second-timer">
+
+            </div>
+
+            <div class="Second-img-container">
+                <img src="Assets/Movie-posters/Doraemon.jpg">
+            </div>
+
+        </section>
+
+        <div class="Reserve-container">
+            <h2>
+                <a href="Reservation_Doraemon.php">RESERVE</a>
+            </h2>
+        </div>
+
+    </section>
+
+    <!-- THIRD MOVIE -->
+    <section class="Third-Movie">
+
+        <section class="Third">
+
+            <div class="Third-timer">
+
+            </div>
+
+            <div class="Third-img-container">
+                <img src="Assets/Movie-posters/Insidious.jpg">
+            </div>
+
+        </section>
+
+        <div class="Reserve-container">
+            <h2>
+                <a href="Reservation_Insidious.php">RESERVE</a>
+            </h2>
+        </div>
+
+    </section>
+
+    <!-- FOURTH MOVIE -->
+    <section class="Fourth-Movie">
+
+        <section class="Fourth">
+
+            <div class="Fourth-timer">
+
+            </div>
+
+            <div class="Fourth-img-container">
+                <img src="Assets/Movie-posters/Moana.jpg">
+            </div>
+
+        </section>
+
+        <div class="Reserve-container">
+            <h2>
+                <a href="Reservation_Moana.php">RESERVE</a>
+            </h2>
+        </div>
+
+    </section>
+
+    <!-- FIFTH MOVIE -->
+    <section class="Fifth-Movie">
+
+        <section class="Fifth">
+
+            <div class="Fifth-timer">
+
+            </div>
+
+            <div class="Fifth-img-container">
+                <img src="Assets/Movie-posters/Odyssey.jpg">
+            </div>
+
+        </section>
+
+        <div class="Reserve-container">
+            <h2>
+                <a href="Reservation_Odyssey.php">RESERVE</a>
+            </h2>
+        </div>
+
+    </section>
+
+    <!-- SIXTH MOVIE -->
+    <section class="Sixth-Movie">
+
+        <section class="Sixth">
+
+            <div class="Sixth-timer">
+
+            </div>
+
+            <div class="Sixth-img-container">
+                <img src="Assets/Movie-posters/Spiderman.jpg">
+            </div>
+
+        </section>
+
+        <div class="Reserve-container">
+            <h2>
+                <a href="Reservation_Spiderman.php">RESERVE</a>
+            </h2>
+        </div>
+
+    </section>
+
+</section>
+    </main>
